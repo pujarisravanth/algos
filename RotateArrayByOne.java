5927d588d00f1ca8dd2f414a51e798e2fba168e1
pujarisravanth <pujarisravanth@gmail.com>
2021-12-05 11:00:58 +0530

    adding rotate array by one element

---
+++ RotateArrayByOne.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class RotateArrayByOne {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        
+        rotateArrayByOne(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void rotateArrayByOne(int[] arr) {
+        int temp = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = temp;
+    }
+}
