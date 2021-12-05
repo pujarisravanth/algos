6cf4e5326bbfc64983ac7bb02f53d5f5562e512d
pujarisravanth <pujarisravanth@gmail.com>
2021-12-05 11:29:51 +0530

    adding two sum

---
+++ TwoSum.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+/**
+ * Leet code problem
+ * with always unique solution exists
+ * need not to be a sorted array
+ */
+public class TwoSum {
+
+    public static void main(String[] args) {
+        int[] nums = { 2, 11, 7, 15 };
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + " " + result[1]);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        HashMap<Integer, Integer> hmap = new HashMap<>();
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (hmap.containsKey(target - nums[i])) {
+                res[0] = hmap.get(target - nums[i]);
+                res[1] = i;
+                break;
+            }
+            hmap.put(nums[i], i);
+        }
+
+        return res;
+    }
+}
