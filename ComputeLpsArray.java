5eb2c503c682b58fa6be6e33510f2060c7117955
pujarisravanth <pujarisravanth@gmail.com>
2021-12-11 19:43:32 +0530

    KMP search with LPS array

---
+++ ComputeLpsArray.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+/**
+ * LPS - Longest prefix which is also a suffix
+ * Used in KMP algorithm
+ */
+
+public class ComputeLpsArray {
+    
+    public static void main(String[] args) {
+        String pattern = "AAACAAAAAC";
+        int[] lps = computeLpsArray(pattern);
+        System.out.println("LPS array: " + Arrays.toString(lps));
+    }
+
+    public static int[] computeLpsArray(String pattern) {
+        int[] lps = new int[pattern.length()];
+        
+        int len = 0;
+        int i = 1;
+        int n = pattern.length();
+        lps[0] = 0;
+
+        while (i < n) {
+            if (pattern.charAt(i) == pattern.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                // For cases like AAACAAAA, when we have to backtrack
+                if (len != 0) {
+                    len = lps[len - 1];
+                }
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return lps;
+    }
+}
---
+++ KmpPatternSearching.java
@@ -0,0 +1,41 @@
+/**
+ * KMP Pattern Searching
+ * Also known as Knuth-Morris-Pratt algorithm
+ */
+
+public class KmpPatternSearching {
+
+    public static void main(String[] args) {
+        String text = "AABAACAADAABAABA";
+        String pattern = "AABA";
+
+        int[] lps = ComputeLpsArray.computeLpsArray(pattern);
+        KmpSearch(text, pattern, lps);
+    }
+
+    public static void KmpSearch(String text, String pattern, int[] lps) {
+        int i = 0;
+        int j = 0;
+
+        int n = text.length();
+        int m = pattern.length();
+
+        while (i < n) {
+            if (text.charAt(i) == pattern.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                if (j == 0) {
+                    i++;
+                } else {
+                    j = lps[j - 1];
+                }
+            }
+
+            if (j == m) {
+                System.out.println("Pattern found at index " + (i - j));
+                j = lps[j - 1];
+            }
+        }
+    }
+}
