77042bc864048736be877d8b7cbc58ca72a79975
pujarisravanth <pujarisravanth@gmail.com>
2022-01-05 15:11:10 +0530

    fixing bruteforce lps to show full length

--- LongestPalindromeSubstring1.java
+++ LongestPalindromeSubstring1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
  * Brute force way of computing LPS array.
  */
 public class LongestPalindromeSubstring1 {
-    
+
     public static void main(String[] args) {
         String s = "abcbabcbabcba";
         int[] lps = computeLPS(s);
@@ -13,15 +13,15 @@ public class LongestPalindromeSubstring1 {
 
     public static int[] computeLPS(String s) {
         int n = s.length();
-        int lps[] = new int[2*n + 1];
+        int lps[] = new int[2 * n + 1];
         lps[0] = 0;
 
-        for (int i = 1; i < 2*n + 1; i++) {
-            lps[i] = 0;
+        for (int i = 1; i < 2 * n + 1; i++) {
+            lps[i] = (i % 2 == 1) ? -1 : 0;
             int a = (i - 1) / 2;
             int b = i / 2;
             while (a >= 0 && b < n && s.charAt(a) == s.charAt(b)) {
-                lps[i]++;
+                lps[i] += 2;
                 a--;
                 b++;
             }
