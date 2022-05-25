4649d2c26b8186268dd5e6a9b8a35bccf142079c
pujarisravanth <pujarisravanth@gmail.com>
2022-05-25 10:57:11 +0530

    adding remove duplicate from sorted array

--- RomanToInteger.java
+++ RomanToInteger.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
 /**
  * Leet Code Problem #13: Roman to Integer
  * https://leetcode.com/problems/roman-to-integer/
@@ -11,39 +14,24 @@ public class RomanToInteger {
     }
 
     public static int romanToInt(String s) {
-        int res = 0;
-        int prev = getIntValue(s.charAt(0));
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
-        for (int i = 1; i < s.length(); i++) {
-            int curr = getIntValue(s.charAt(i));
-            if (curr > prev) {
-                res -= prev;
-            } else {
-                res += prev;
-            }
+        int prev = map.get(s.charAt(s.length() - 1));
+        int res = prev;
+
+        for (int i = s.length() - 2; i >= 0; i--) {
+            int curr = map.get(s.charAt(i));
+            res += curr * ((curr < prev) ? -1 : 1);
             prev = curr;
         }
-        res += prev;
 
         return res;
-    }
-
-    private static int getIntValue(char c) {
-        switch (c) {
-            case 'M':
-                return 1000;
-            case 'D':
-                return 500;
-            case 'C':
-                return 100;
-            case 'L':
-                return 50;
-            case 'X':
-                return 10;
-            case 'V':
-                return 5;
-            default:
-                return 1;
-        }
     }
 }
--- Test.java
+++ Test.java
@@ -1,6 +1,34 @@
 public class Test {
 
     public static void main(String[] args) {
-        String s = "ABC";
+        String[] strs = { "flower", "flow", "flight" };
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder("");
+
+        if (strs.length == 0) {
+            return sb.toString();
+        }
+
+        int minLength = Integer.MAX_VALUE;
+        for (int i = 0; i < strs.length; i++) {
+            if (strs[i].length() < minLength) {
+                minLength = strs[i].length();
+            }
+        }
+
+        for (int i = 0; i < minLength; i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (strs[j].charAt(i) != c) {
+                    return sb.toString();
+                }
+            }
+            sb.append(c);
+        }
+
+        return sb.toString();
     }
 }
