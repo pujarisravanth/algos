e780d4b6e5f0d7abd5afac1c4d42a2525219fe9a
sravanthpujari <sravanth.pujari@digicert.com>
2022-10-30 02:04:37 +0530

    adding two sum

---
+++ TwoSum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Leet code problem #1
+ * https://leetcode.com/problems/two-sum/
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] {map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
