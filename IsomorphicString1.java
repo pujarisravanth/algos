2b854e6db9c65dafa4f482e58ee5c8a77c8010bb
pujarisravanth <pujarisravanth@gmail.com>
2023-01-30 23:47:39 +0530

    adding word pattern question

--- IsomorphicString1.java
+++ IsomorphicString1.java
@@ -12,20 +12,21 @@ public class IsomorphicString1 {
     }
 
     public static boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
+        if (s.length() != t.length())
+            return false;
 
         int[] arr1 = new int[256], arr2 = new int[256];
         Arrays.fill(arr1, -1);
         Arrays.fill(arr2, -1);
-        
-        for (int i=0; i<s.length(); i++) {
+
+        for (int i = 0; i < s.length(); i++) {
             char c1 = s.charAt(i), c2 = t.charAt(i);
 
             if (arr1[c1] == -1 && arr2[c2] == -1) {
                 arr1[c1] = c2;
                 arr2[c2] = c1;
-            }
-            else if (arr1[c1] != c2 || arr2[c2] != c1) return false;
+            } else if (arr1[c1] != c2 || arr2[c2] != c1)
+                return false;
         }
 
         return true;
---
+++ WordPattern.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+
+/**
+ * Leetcode problem #290, Word Pattern
+ */
+
+public class WordPattern {
+    public static void main(String[] args) {
+        String pattern = "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccdd";
+        String s = "s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s s t t";
+
+        boolean res = wordPattern(pattern, s);
+        System.out.println(res);
+    }
+
+    public static boolean wordPattern(String pattern, String s) {
+        String[] strArr = s.split(" ");
+
+        if (pattern.length() != strArr.length) {
+            return false;
+        }
+
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<String, Integer> map2 = new HashMap<>();
+
+        for (int i = 0; i < strArr.length; i++) {
+            char c = pattern.charAt(i);
+            String w = strArr[i];
+
+            if (!map1.containsKey(c)) {
+                map1.put(c, i);
+            }
+
+            if (!map2.containsKey(w)) {
+                map2.put(w, i);
+            }
+
+            if (!map1.get(c).equals(map2.get(w))) {
+                System.out.printf("%c : %d, %s : %d", c, map1.get(c), w, map2.get(w));
+                System.out.println();
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
