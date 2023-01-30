443e24570979dbc70f383f7f56047053ad0f6005
pujarisravanth <pujarisravanth@gmail.com>
2023-01-30 17:36:47 +0530

    adding some modifications

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -54,3 +54,22 @@ public class BinarySearchTree {
         printInorderTraversal(node.right);
     }
 }
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node() {
+    }
+
+    Node(int val) {
+        this.val = val;
+    }
+
+    Node(int x, Node left, Node right) {
+        this.val = x;
+        this.left = left;
+        this.right = right;
+    }
+}
--- MergeTwoBST.java
+++ MergeTwoBST.java
@@ -70,3 +70,22 @@ public class MergeTwoBST {
         storeInOrderTraversal(node.right, res);
     }
 }
+
+class Node {
+    int val;
+    Node left;
+    Node right;
+
+    Node() {
+    }
+
+    Node(int val) {
+        this.val = val;
+    }
+
+    Node(int x, Node left, Node right) {
+        this.val = x;
+        this.left = left;
+        this.right = right;
+    }
+}
