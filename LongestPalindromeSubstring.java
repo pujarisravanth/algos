f34d3f7a81f1190801ea501ac1fad7ee1c85fc41
pujarisravanth <pujarisravanth@gmail.com>
2023-02-21 20:38:27 +0530

    adding single non duplicate element

--- LongestPalindromeSubstring.java
+++ LongestPalindromeSubstring.java
@@ -1,3 +1,7 @@
+/**
+ * Leetcode problem #5, Longest Palindromic Substring
+ * https://leetcode.com/problems/longest-palindromic-substring/
+ */
 public class LongestPalindromeSubstring {
     public static void main(String[] args) {
         String s = "babad";
@@ -6,21 +10,22 @@ public class LongestPalindromeSubstring {
     }
 
     public static String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
+        if (s == null || s.length() < 1)
+            return "";
 
         int start = 0, end = 0;
-        for (int i=0; i<s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i+1);
+            int len2 = expandAroundCenter(s, i, i + 1);
             int len = Math.max(len1, len2);
 
             if (len > end - start + 1) {
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
             }
-         }
+        }
 
-         return s.substring(start, end + 1);
+        return s.substring(start, end + 1);
     }
 
     public static int expandAroundCenter(String s, int l, int r) {
