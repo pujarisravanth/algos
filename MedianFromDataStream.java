1bfac03144b5dda5631ab53bbbbb87e81376437f
pujarisravanth <pujarisravanth@gmail.com>
2023-02-26 16:58:43 +0530

    median of data stream solution

---
+++ MedianFromDataStream.java
@@ -0,0 +1,59 @@
+import java.util.PriorityQueue;
+
+import javax.print.attribute.standard.Media;
+
+/**
+ * Leetcode problem #295, Find Median from Data Stream
+ * https://leetcode.com/problems/find-median-from-data-stream/
+ */
+
+public class MedianFromDataStream {
+    public static void main(String[] args) {
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.addNum(1); // arr = [1]
+        medianFinder.addNum(2); // arr = [1, 2]
+        System.out.println(medianFinder.findMedian()); // return 1.5 (i.e., (1 + 2) / 2)
+        medianFinder.addNum(3); // arr[1, 2, 3]
+        System.out.println(medianFinder.findMedian()); // return 2.0
+    }
+}
+
+class MedianFinder {
+    private PriorityQueue<Integer> minHeap;
+    private PriorityQueue<Integer> maxHeap;
+    private int size;
+
+    public MedianFinder() {
+        this.minHeap = new PriorityQueue<>((a, b) -> a - b);
+        this.maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        this.size = 0;
+    }
+
+    public void addNum(int num) {
+        if (maxHeap.isEmpty()) {
+            maxHeap.add(num);
+            size++;
+            return;
+        }
+
+        if (maxHeap.peek() >= num) {
+            maxHeap.add(num);
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.add(maxHeap.poll());
+            }
+        } else {
+            minHeap.add(num);
+            if (minHeap.size() > maxHeap.size()) {
+                maxHeap.add(minHeap.poll());
+            }
+        }
+
+        size++;
+    }
+
+    public double findMedian() {
+        if (size % 2 == 0)
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        return maxHeap.peek() * 1.0;
+    }
+}
