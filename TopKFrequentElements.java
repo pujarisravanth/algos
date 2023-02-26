de4195ccf4681441d2844d3edfa929555fe5de6e
pujarisravanth <pujarisravanth@gmail.com>
2023-02-26 18:31:52 +0530

    top k frequent elements

---
+++ TopKFrequentElements.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.PriorityQueue;
+
+public class TopKFrequentElements {
+    public static void main(String[] args) {
+        int[] nums = { 1, 1, 1, 2, 2, 3 };
+        int[] res = topKFrequent(nums, 2);
+        System.out.println(Arrays.toString(res));
+    }
+
+    public static int[] topKFrequent(int[] nums, int k) {
+        if (k == nums.length)
+            return nums;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+        for (int val : map.keySet()) {
+            minHeap.add(val);
+            if (minHeap.size() > k)
+                minHeap.poll();
+        }
+
+        int[] res = new int[k];
+        for (int i = k - 1; i >= 0; i--) {
+            res[i] = minHeap.poll();
+        }
+
+        return res;
+    }
+}
