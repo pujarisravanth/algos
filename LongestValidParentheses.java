10a86b98236727f8e3c0b94449238cf532e78262
pujarisravanth <pujarisravanth@gmail.com>
2023-03-04 23:06:15 +0530

    adding formatting

--- LongestValidParentheses.java
+++ LongestValidParentheses.java
@@ -5,25 +5,26 @@ import java.util.Stack;
  * https://leetcode.com/problems/longest-valid-parentheses/
  */
 public class LongestValidParentheses {
-    public static void main(String[] args) {
-        String s = ")()())";
-        int len = longestValidParentheses(s);
-        System.out.println(len);
+
+  public static void main(String[] args) {
+    String s = ")()())";
+    int len = longestValidParentheses(s);
+    System.out.println(len);
+  }
+
+  public static int longestValidParentheses(String s) {
+    int l = 0, len = 0;
+    Stack<Integer> st = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == ')' && !st.isEmpty() && s.charAt(st.peek()) == '(') {
+        st.pop();
+        l = st.isEmpty() ? -1 : st.peek();
+        len = Math.max(len, i - l);
+        continue;
+      }
+      st.push(i);
     }
 
-    public static int longestValidParentheses(String s) {
-        int l = 0, len = 0;
-        Stack<Integer> st = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ')' && !st.isEmpty() && s.charAt(st.peek()) == '(') {
-                st.pop();
-                l = st.isEmpty() ? -1 : st.peek();
-                len = Math.max(len, i - l);
-                continue;
-            }
-            st.push(i);
-        }
-
-        return len;
-    }
+    return len;
+  }
 }
