334d512401053c146c2c177aac1407cbaf2ff1ea
sravanthpujari <sravanth.pujari@digicert.com>
2023-04-16 09:45:42 +0530

    LRU cache using linked hashmaps

---
+++ LRUCacheM1.java
@@ -0,0 +1,38 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LRUCacheM1 {
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));
+        cache.put(3, 3);
+        System.out.println(cache.get(2));
+        cache.put(4, 4);
+        System.out.println(cache.get(3));
+        System.out.println(cache.get(4));
+    }
+}
+
+class LRUCache extends LinkedHashMap<Integer, Integer> {
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        super(capacity, 0.75F, true);
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        return super.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        super.put(key, value);
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> entry) {
+        return super.size() > this.capacity;
+    }
+}
