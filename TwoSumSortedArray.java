01b8d108a08d1e8290f100743de14bc2f30affcd
sravanthpujari <sravanth.pujari@digicert.com>
2024-06-24 23:28:11 +0530

    adding small fixes

--- TwoSumSortedArray.java
+++ TwoSumSortedArray.java
@@ -19,14 +19,11 @@ public class TwoSumSortedArray {
 
     public static int[] twoSum(int[] numbers, int target) {
         int l = 0, r = numbers.length - 1;
-        int[] res = new int[2];
 
         while (l < r) {
             int sum = numbers[l] + numbers[r];
             if (sum == target) {
-                res[0] = l + 1;
-                res[1] = r + 1;
-                break;
+                return new int[] { l + 1, r + 1 };
             } else if (sum > target) {
                 r--;
             } else {
@@ -34,6 +31,6 @@ public class TwoSumSortedArray {
             }
         }
 
-        return res;
+        return new int[2];
     }
 }
