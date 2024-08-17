504fecc916fa760ba47e048769e0b1e22332958e
pujarisravanth <pujarisravanth@gmail.com>
2024-08-18 02:32:22 +0530

    making minor modifications to longest substring

--- LongestSubstringWithoutRepeating.java
+++ LongestSubstringWithoutRepeating.java
@@ -23,8 +23,7 @@ public class LongestSubstringWithoutRepeating {
         HashMap<Character, Integer> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             if (map.containsKey(s.charAt(i))) {
-                int index = map.get(s.charAt(i));
-                start = index >= start ? index + 1 : start;
+                start = Math.max(s.charAt(i) + 1, start);
             }
             map.put(s.charAt(i), i);
             len = Math.max(i - start + 1, len);
