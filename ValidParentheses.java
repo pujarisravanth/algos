180d959c6d795cc326793622d191b466d63d8caf
pujarisravanth <pujarisravanth@gmail.com>
2024-09-15 02:49:07 +0530

    adding comments to valid parentheses

--- ValidParentheses.java
+++ ValidParentheses.java
@@ -1,5 +1,9 @@
 import java.util.Stack;
 
+/**
+ * Leetcode problem #20, Valid Parentheses
+ * https://leetcode.com/problems/valid-parentheses/
+ */
 public class ValidParentheses {
 
     public static void main(String[] args) {
@@ -7,25 +11,30 @@ public class ValidParentheses {
         System.out.println(isValid(s));
     }
 
-    public static boolean isValid(String s) {   
-        if (s.length() == 0) return true;
+    public static boolean isValid(String s) {
+        if (s.length() == 0)
+            return true;
 
         Stack<Character> st = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '{' || c == '(' || c == '[') {
                 st.push(c);
-            }
-            else {
-                if (st.empty()) return false;
+            } else {
+                if (st.empty())
+                    return false;
                 char last_char = st.pop();
-                if (last_char == '{' && c == '}') continue;
-                else if (last_char == '(' && c == ')') continue;
-                else if (last_char == '[' && c == ']') continue;
-                else return false;
+                if (last_char == '{' && c == '}')
+                    continue;
+                else if (last_char == '(' && c == ')')
+                    continue;
+                else if (last_char == '[' && c == ']')
+                    continue;
+                else
+                    return false;
             }
         }
-  
+
         return st.empty();
     }
 }
